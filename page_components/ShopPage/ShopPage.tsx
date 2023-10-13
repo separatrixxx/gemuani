defe5b429e2a9b92de9df2835cc885c14770902c
Nikita <nikitalochmatov@gmail.com>
2023-10-13 19:24:04 +0300

    update

--- page_components/ShopPage/ShopPage.tsx
+++ page_components/ShopPage/ShopPage.tsx
@@ -22,17 +22,17 @@ export const ShopPage = (): JSX.Element => {
 				<Header />
 				<div className={styles.shopBody}>
 					<Htag tag='xxl' className={styles.shopTitle}>{setLocale(router.locale).shop_page.title1}</Htag>
-					<a rel='noreferrer' target='_blank' href='https://www.amazon.com/s?k=GEMUANI&ref=bl_dp_s_web_0'>
+					<a className={styles.link} rel='noreferrer' target='_blank' href='https://www.amazon.com/s?k=GEMUANI&ref=bl_dp_s_web_0'>
 						<Htag tag='xl' className={styles.shopLink}>
 							<Amazon />
 						</Htag>
 					</a>
-					<a rel='noreferrer' target='_blank' href='https://ufv9.adj.st/?adjust_deeplink=glovoapp%3A%2F%2Fopen%3Flink_type%3Dstore%26store_id%3D398663&adjust_t=s321jkn'>
+					<a className={styles.link} rel='noreferrer' target='_blank' href='https://ufv9.adj.st/?adjust_deeplink=glovoapp%3A%2F%2Fopen%3Flink_type%3Dstore%26store_id%3D398663&adjust_t=s321jkn'>
 						<Htag tag='xl' className={styles.shopLink}>
 							<Glovo />
 						</Htag>
 					</a>
-					<a rel='noreferrer' target='_blank' href='https://wolt.com/geo/tbilisi/venue/wolt-market-marijani?search=%E1%83%92%E1%83%94%E1%83%9B%E1%83%A3%E1%83%90%E1%83%9C%E1%83%98'>
+					<a className={styles.link} rel='noreferrer' target='_blank' href='https://wolt.com/geo/tbilisi/venue/wolt-market-marijani?search=%E1%83%92%E1%83%94%E1%83%9B%E1%83%A3%E1%83%90%E1%83%9C%E1%83%98'>
 						<Htag tag='xl' className={styles.shopLink}>
 							<Wolt />
 						</Htag>
