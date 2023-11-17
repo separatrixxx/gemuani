059c47063290571247068e8e4ee8893c524e06b3
separatrixxx <nikitalochmatov@gmail.com>
2023-11-17 11:37:47 +0300

    update

--- page_components/KiwiFarmPage/KiwiFarmPage.tsx
+++ page_components/KiwiFarmPage/KiwiFarmPage.tsx
@@ -6,6 +6,7 @@ import { Footer } from 'components/Footer/Footer';
 import { setLocale } from 'helpers/locale.helper';
 import { TopElem } from 'components/TopElem/TopElem';
 import { TopButton } from 'components/MainPageComponents/TopButton/TopButton';
+import { AboutImageBlock } from 'components/AboutPageComponents/AboutImageBlock/AboutImageBlock';
 
 
 export const KiwiFarmPage = (): JSX.Element => {
@@ -22,6 +23,7 @@ export const KiwiFarmPage = (): JSX.Element => {
 					<Htag tag='l'>{setLocale(router.locale).kiwi_farm_page.text1}</Htag>
 					<Htag tag='l'>{setLocale(router.locale).kiwi_farm_page.text2}</Htag>
 					<Htag tag='l'>{setLocale(router.locale).kiwi_farm_page.text3}</Htag>
+					<AboutImageBlock image='/KiwiImage1.webp' alt='kiwi farm image 1' />
 					<Htag tag='xl' className={styles.title}>{setLocale(router.locale).kiwi_farm_page.title2}</Htag>
 					<Htag tag='l'>{setLocale(router.locale).kiwi_farm_page.text4}</Htag>
 					<Htag tag='l'>{setLocale(router.locale).kiwi_farm_page.text5}</Htag>
