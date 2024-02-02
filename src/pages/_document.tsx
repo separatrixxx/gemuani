7489a50c9285c51a472eaa21c14a310018622106
separatrixxx <nikitalochmatov@gmail.com>
2024-02-02 13:24:07 +0300

    update

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,6 +8,17 @@ export default function Document() {
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link href="https://fonts.googleapis.com/css2?family=Agbalumo&family=Inter:wght@200;300;400;500;600;700;800;900&family=Nunito+Sans:opsz,wght@6..12,200;6..12,300;6..12,400;6..12,500;6..12,600;6..12,700;6..12,800;6..12,900&family=Playfair+Display:wght@400;500;600;700;800;900&display=swap" rel="stylesheet" />     
       </Head>
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-8L6JH5P8S0"></script>
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-8L6JH5P8S0');
+          `,
+        }}
+      />
       <body>
         <Main />
         <NextScript />
