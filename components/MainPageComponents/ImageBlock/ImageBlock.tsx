1d9bc3228c78050c4f87489076340ed93170c73e
separatrixxx <nikitalochmatov@gmail.com>
2024-02-05 16:45:58 +0300

    update

--- components/MainPageComponents/ImageBlock/ImageBlock.tsx
+++ components/MainPageComponents/ImageBlock/ImageBlock.tsx
@@ -36,17 +36,14 @@ export const ImageBlock = ({ text, image, imageMobile, ticker }: ImageBlockProps
 	} else {
 		return (
 			<div id='imageBlock' className={styles.imageBlock}>
-				<div className={styles.imageDiv}>
-					<Image className={styles.image} draggable='false'
-						loader={() => image}
-						src={width > 1024 ? image : imageMobile}
-						alt='image'
-						width={1}
-						height={1}
-						unoptimized={true}
-						loading="lazy"
-					/>
-				</div>
+				<Image className={styles.image} draggable='false'
+					loader={() => width > 1024 ? image : imageMobile}
+					src={width > 1024 ? image : imageMobile}
+					alt='image'
+					width={1}
+					height={1}
+					unoptimized={true}
+				/>
 				<Htag tag='xxl' className={styles.text}>{text}</Htag>
 			</div>
 		);
