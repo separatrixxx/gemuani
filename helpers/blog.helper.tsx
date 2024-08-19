4265c7568699ec446ab467321db8c2bd66042462
Nikita Lokhmatov <nikitalochmatov@gmail.com>
2024-08-19 12:16:25 +0300

    Update blog.helper.tsx

--- helpers/blog.helper.tsx
+++ helpers/blog.helper.tsx
@@ -7,14 +7,22 @@ import { ToastError, ToastSuccess } from "components/Common/Toast/Toast";
 export function getBlog(): Blog[] {
     const blog1: Blog = {
         id: '0',
+        image: ['/BlogImg5_1.webp', '/BlogImg5_2.webp', '/BlogImg5_3.webp', '/BlogImg5_4.webp', '/BlogImg5_5.webp', '/BlogImg5_6.webp'],
+        title: 'USAID Delegation Visits Gemuani Plant',
+        text: "Our Gemuani plant was recently visited by a delegation from USAID. During the visit, we discussed prospects for further cooperation and potential investments in the development of our production. We thank USAID for their interest and support of our activities. This collaboration will allow us to expand our production capacities, implement innovative technologies, and improve the quality of our products.",
+        date: '2024-08-19',
+    };
+
+    const blog2: Blog = {
+        id: '1',
         image: ['/BlogImg2.webp', '/BlogImg2_1.webp', '/BlogImg2_2.webp'],
         title: 'GOOLFOOD 2024',
         text: "Gemuani, the leader in Georgian freeze-dried fruits and vegetables 🍇🍎, are pleased to join GULFOOD 2024 in Dubai 🌍 from 19 to 23 February 2024, a major global food and beverage exhibition 🥘🌐.\nGemuani will present its high-quality freeze-dried Georgian products 🍒🥦 at GULFOOD 2024, presenting Georgia's culinary heritage and innovation in the country pavilion👩‍🍳.\nOur presence at GULFOOD 2024 reflects our commitment to promoting Georgian products around the world 🌏 and our desire to explore new opportunities and share our culinary passion ❤️🍴.\nJoin us at GULFOOD 2024, where tradition meets innovation, and experience the taste of Georgia with Gemuani 🤝🍽.",
         date: '2024-02-19',
     };
 
-    const blog2: Blog = {
-        id: '1',
+    const blog3: Blog = {
+        id: '2',
         image: ['/BlogImg3.webp'],
         title: 'Grüne Woche 2024',
         text: "Our company Gemuani is participating in the prestigious Grüne Woche 2024! 🌱🚜 A hub for innovation in food, agriculture and horticulture, this international exhibition is perfectly aligned with our values ​​​​and goals. Join us from 19 to 28 January in Berlin as we showcase our products and network with global industry leaders. 🌍",
@@ -24,16 +32,16 @@ export function getBlog(): Blog[] {
         }
     };
 
-    const blog3: Blog = {
-        id: '2',
+    const blog4: Blog = {
+        id: '3',
         image: ['/BlogImg4.webp'],
         title: 'Partnership with Pigo',
         text: "Mr. Goran, CEO of Pigo company, very often visits our factory to show what high-level product can be done, visit was with new possible buyer of Pigo. 🏭🤝 Wish to our partner success. 🌟 We are happy to choose Pigo, Italy company as our partner.",
         date: '2023-12-07',
     };
 
-    const blog4: Blog = {
-        id: '3',
+    const blog5: Blog = {
+        id: '4',
         image: ['/BlogImg1.webp', '/BlogImg1_1.webp', '/BlogImg1_2.webp'],
         title: 'OPERA CROWN awards',
         text: 'The winners of the III Tbilisi International Opera Crown Voice Competition, held on November 25, 2023, were announced as follows: \n\n1st Prize - Elmina Hasan (Georgia-Azerbaijan), awarded by Tamar Tsulukidze, Head of the Department for the Promotion of the Creative Process at the Ministry of Culture.\n2nd Prize - Ana Palavandishvili (Georgia), awarded by Abdulaziz Hassan Zakri, founder and CEO of Al-Mutafarrda.\n3rd Prize - Dinmukhamed Koshkinbayev (Kazakhstan), awarded by Archil Jaliashvili, founder of Gemuani.\nTbilisi State Opera and Ballet Theater Prize: Alikhan Zeinolla (Kazakhstan), presented by Badri Maisuradze, the artistic director of the theater.\n\nCongratulations to all the participants for their outstanding performances and dedication to the art of opera. Your talent and hard work are truly inspiring, and we look forward to seeing your future contributions to the world of music. Keep up the great work!',
@@ -46,15 +54,7 @@ export function getBlog(): Blog[] {
         }
     };
 
-    const blog5: Blog = {
-        id: '4',
-        image: ['/BlogImg5_1.webp', '/BlogImg5_2.webp', '/BlogImg5_3.webp', '/BlogImg5_4.webp', '/BlogImg5_5.webp', '/BlogImg5_6.webp'],
-        title: 'USAID Delegation Visits Gemuani Plant',
-        text: "Our Gemuani plant was recently visited by a delegation from USAID. During the visit, we discussed prospects for further cooperation and potential investments in the development of our production. We thank USAID for their interest and support of our activities. This collaboration will allow us to expand our production capacities, implement innovative technologies, and improve the quality of our products.",
-        date: '2024-08-19',
-    };
-
-    return [blog5, blog1, blog2, blog3, blog4];
+    return [blog1, blog2, blog3, blog4, blog5];
 }
 
 // export async function getBlog(setBlog: (e: any) => void) {
