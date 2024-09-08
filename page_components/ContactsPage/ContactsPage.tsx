a14e46f4d5d8f92e0f04bf38a89f63d5768c8ff0
separatrixxx <nikitalochmatov@gmail.com>
2024-09-08 16:58:40 +0300

    update contacts

--- page_components/ContactsPage/ContactsPage.tsx
+++ page_components/ContactsPage/ContactsPage.tsx
@@ -35,7 +35,7 @@ export const ContactsPage = (): JSX.Element => {
 						<Htag tag='xxl' className={styles.contactsTitle}>{setLocale(router.locale).contacts_page.title2}</Htag>
 						<div className={styles.infoBlock}>
 							<Htag tag='l'>{setLocale(router.locale).contacts_page.address}</Htag>
-							<a target='_blank' rel="noreferrer" href="https://www.google.com/maps/place/+89+Irakli+Abashidze+Street,+T'bilisi,+%D0%93%D1%80%D1%83%D0%B7%D0%B8%D1%8F/@41.709055,44.7531785,19z/data=!3m1!4b1!4m5!3m4!1s0x40447337db44d409:0x3c789f7363075fc7!8m2!3d41.709054!4d44.7538222?entry=ttu">
+							<a target='_blank' rel="noreferrer" href="https://www.google.com/maps/place/Gemuani/@42.5843148,41.9265745,321m/data=!3m1!1e3!4m12!1m5!3m4!2zNDLCsDM1JzAzLjYiTiA0McKwNTUnMzguOCJF!8m2!3d42.584343!4d41.927447!3m5!1s0x405c220bcfb9215f:0xae61153095aed128!8m2!3d42.584323!4d41.9282474!16s%2Fg%2F11fy2z37f1?entry=ttu&g_ep=EgoyMDI0MDkwNC4wIKXMDSoASAFQAw%3D%3D">
 								<Htag tag='l' className={styles.view}>
 									{setLocale(router.locale).contacts_page.view_on_map}
 									<span className={styles.arrow}><Arrow /></span>
